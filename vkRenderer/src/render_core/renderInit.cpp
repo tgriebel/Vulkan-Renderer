808036bca2a599fc016df489cafd18d8618219a8
Thomas Griebel <631199+tgriebel@users.noreply.github.com>
2023-03-01 08:55:09 -0600

    Shadow maps don't have resource dependencies

--- vkRenderer/src/render_core/renderInit.cpp
+++ vkRenderer/src/render_core/renderInit.cpp
@@ -563,24 +563,6 @@ void Renderer::CreateRenderPasses()
 		subpass.pColorAttachments = &colorAttachmentRef;
 		subpass.pDepthStencilAttachment = &depthAttachmentRef;
 
-		std::array<VkSubpassDependency, 2> dependencies;
-
-		dependencies[ 0 ].srcSubpass = VK_SUBPASS_EXTERNAL;
-		dependencies[ 0 ].dstSubpass = 0;
-		dependencies[ 0 ].srcStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
-		dependencies[ 0 ].dstStageMask = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
-		dependencies[ 0 ].srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
-		dependencies[ 0 ].dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
-		dependencies[ 0 ].dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
-
-		dependencies[ 1 ].srcSubpass = 0;
-		dependencies[ 1 ].dstSubpass = VK_SUBPASS_EXTERNAL;
-		dependencies[ 1 ].srcStageMask = VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
-		dependencies[ 1 ].dstStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
-		dependencies[ 1 ].srcAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
-		dependencies[ 1 ].dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
-		dependencies[ 1 ].dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
-
 		std::array<VkAttachmentDescription, 2> attachments = { colorAttachment, depthAttachment };
 		VkRenderPassCreateInfo renderPassInfo{ };
 		renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
@@ -588,8 +570,7 @@ void Renderer::CreateRenderPasses()
 		renderPassInfo.pAttachments = attachments.data();
 		renderPassInfo.subpassCount = 1;
 		renderPassInfo.pSubpasses = &subpass;
-		renderPassInfo.dependencyCount = static_cast<uint32_t>( dependencies.size() );
-		renderPassInfo.pDependencies = dependencies.data();
+		renderPassInfo.dependencyCount = 0;
 
 		shadowPassState.clearColor = vec4f( 1.0f, 1.0f, 1.0f, 1.0f );
 		shadowPassState.clearDepth = 1.0f;
