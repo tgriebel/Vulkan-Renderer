06f87027b3b384337d0a0ad6c7b647eda5253ec9
Thomas Griebel <631199+tgriebel@users.noreply.github.com>
2023-05-23 08:02:14 -0500

    Move out stuff from the main renderer file

---
+++ vkRenderer/src/render_binding/descriptorWrites.cpp
@@ -0,0 +1,187 @@
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <map>
+#include "../render_core/renderer.h"
+#include <scene/scene.h>
+#include <scene/entity.h>
+#include <sstream>
+#include "../render_core/debugMenu.h"
+#include "../render_core/gpuImage.h"
+#include "../render_state/rhi.h"
+#include "bindings.h"
+
+union descriptorInfo_t
+{
+	VkDescriptorBufferInfo	bufferInfo;
+	VkDescriptorImageInfo	imageInfo;
+};
+
+class DescriptorWritesBuilder
+{
+private:
+	static const uint32_t MaxPoolSize = 100;
+	uint32_t							bufferInfoPoolFreeList = 0;
+	uint32_t							imageInfoPoolFreeList = 0;
+	uint32_t							bufferInfoArrayPoolFreeList = 0;
+	uint32_t							imageInfoArrayPoolFreeList = 0;
+	VkDescriptorBufferInfo				bufferInfoPool[ MaxPoolSize ];
+	VkDescriptorImageInfo				imageInfoPool[ MaxPoolSize ];
+	std::vector<VkDescriptorBufferInfo>	bufferInfoArrayPool[ MaxPoolSize ];
+	std::vector<VkDescriptorImageInfo>	imageInfoArrayPool[ MaxPoolSize ];
+public:
+	DescriptorWritesBuilder()
+	{
+		const uint32_t reservationSize = 128;
+		for ( uint32_t i = 0; i < MaxPoolSize; ++i )
+		{
+			bufferInfoArrayPool[ i ].reserve( reservationSize );
+			imageInfoArrayPool[ i ].reserve( reservationSize );
+		}
+		bufferInfoPoolFreeList = 0;
+		imageInfoPoolFreeList = 0;
+		bufferInfoArrayPoolFreeList = 0;
+		imageInfoArrayPoolFreeList = 0;
+	}
+
+	void Reset()
+	{
+		for ( uint32_t i = 0; i < MaxPoolSize; ++i )
+		{
+			bufferInfoArrayPool[ i ].resize( 0 );
+			imageInfoArrayPool[ i ].resize( 0 );
+		}
+		bufferInfoPoolFreeList = 0;
+		imageInfoPoolFreeList = 0;
+		bufferInfoArrayPoolFreeList = 0;
+		imageInfoArrayPoolFreeList = 0;
+	}
+
+	[[nodiscard]]
+	std::vector<VkDescriptorBufferInfo>& NextBufferInfoArray()
+	{
+		return bufferInfoArrayPool[ bufferInfoArrayPoolFreeList++ ];
+	}
+
+	[[nodiscard]]
+	std::vector<VkDescriptorImageInfo>& NextImageInfoArray()
+	{
+		return imageInfoArrayPool[ imageInfoArrayPoolFreeList++ ];
+	}
+
+	[[nodiscard]]
+	VkDescriptorBufferInfo& NextBufferInfo()
+	{
+		bufferInfoPool[ bufferInfoPoolFreeList ] = {};
+		return bufferInfoPool[ bufferInfoPoolFreeList++ ];
+	}
+
+	[[nodiscard]]
+	VkDescriptorImageInfo& NextImageInfo()
+	{
+		imageInfoPool[ imageInfoPoolFreeList ] = {};
+		return imageInfoPool[ imageInfoPoolFreeList++ ];
+	}
+};
+
+static DescriptorWritesBuilder writeBuilder;
+
+void Renderer::AppendDescriptorWrites( const ShaderBindParms& parms, std::vector<VkWriteDescriptorSet>& descSetWrites )
+{
+	const ShaderBindSet* set = parms.GetSet();
+
+	const uint32_t count = set->Count();
+	descSetWrites.reserve( descSetWrites.size() + count );
+
+	for ( uint32_t i = 0; i < count; ++i )
+	{
+		const ShaderBinding* binding = set->GetBinding( i );
+		const ShaderAttachment* attachment = parms.GetAttachment( binding );
+
+		VkWriteDescriptorSet writeInfo = {};
+		writeInfo.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+		writeInfo.descriptorCount = binding->GetMaxDescriptorCount();
+		writeInfo.dstSet = parms.GetVkObject();
+		writeInfo.descriptorType = vk_GetDescriptorType( binding->GetType() );
+		writeInfo.dstArrayElement = 0;
+		writeInfo.dstBinding = binding->GetSlot();
+
+		if ( attachment->GetType() == ShaderAttachment::type_t::BUFFER ) {
+			const GpuBuffer* buffer = attachment->GetBuffer();
+
+			VkDescriptorBufferInfo& info = writeBuilder.NextBufferInfo();
+			info.buffer = buffer->GetVkObject();
+			info.offset = buffer->GetBaseOffset();
+			info.range = buffer->GetSize();
+
+			assert( info.buffer != nullptr );
+
+			writeInfo.pBufferInfo = &info;
+		}
+		else if ( attachment->GetType() == ShaderAttachment::type_t::IMAGE ) {
+			const Texture* image = attachment->GetImage();
+
+			VkDescriptorImageInfo& info = writeBuilder.NextImageInfo();
+			info.sampler = vk_bilinearSampler;
+			info.imageView = attachment->GetImage()->gpuImage->GetVkImageView();
+			assert( info.imageView != nullptr );
+
+			if ( ( image->info.aspect & ( TEXTURE_ASPECT_DEPTH_FLAG | TEXTURE_ASPECT_STENCIL_FLAG ) ) != 0 ) {
+				info.imageLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL;
+			}
+			else {
+				info.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+			}
+
+			writeInfo.pImageInfo = &info;
+		}
+		else if ( attachment->GetType() == ShaderAttachment::type_t::IMAGE_ARRAY ) {
+			std::vector<VkDescriptorImageInfo>& infos = writeBuilder.NextImageInfoArray();
+
+			const ImageArray& images = *attachment->GetImageArray();
+
+			const uint32_t imageCount = images.Count();
+			infos.resize( imageCount );
+			writeInfo.descriptorCount = imageCount;
+
+			assert( imageCount <= binding->GetMaxDescriptorCount() );
+
+			for ( uint32_t imageIx = 0; imageIx < imageCount; ++imageIx )
+			{
+				const Texture* image = images[ imageIx ];
+				VkDescriptorImageInfo& info = infos[ imageIx ];
+
+				info = {};
+				info.imageView = images[ imageIx ]->gpuImage->GetVkImageView();
+				assert( info.imageView != nullptr );
+
+				if ( ( image->info.aspect & ( TEXTURE_ASPECT_DEPTH_FLAG | TEXTURE_ASPECT_STENCIL_FLAG ) ) != 0 )
+				{
+					info.sampler = vk_depthShadowSampler;
+					info.imageLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL;
+				}
+				else
+				{
+					info.sampler = vk_bilinearSampler;
+					info.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+				}
+			}
+			writeInfo.pImageInfo = infos.data();
+		}
+
+		descSetWrites.push_back( writeInfo );
+	}
+}
+
+
+void Renderer::UpdateFrameDescSet( const int currentImage )
+{
+	writeBuilder.Reset();
+	std::vector<VkWriteDescriptorSet> descriptorWrites;
+	AppendDescriptorWrites( *mainPassState.parms[ currentImage ], descriptorWrites );
+	AppendDescriptorWrites( *shadowPassState.parms[ currentImage ], descriptorWrites );
+	AppendDescriptorWrites( *postPassState.parms[ currentImage ], descriptorWrites );
+	AppendDescriptorWrites( *particleState.parms[ currentImage ], descriptorWrites );
+
+	vkUpdateDescriptorSets( context.device, static_cast<uint32_t>( descriptorWrites.size() ), descriptorWrites.data(), 0, nullptr );
+}
---
+++ vkRenderer/src/render_core/raytracerInterface.h
@@ -0,0 +1,100 @@
+#pragma once
+
+#include "renderer.h"
+#include <scene/scene.h>
+#include <scene/entity.h>
+
+static RtView rtview;
+static RtScene rtScene;
+
+static void BuildRayTraceScene( Scene* scene )
+{
+	rtScene.scene = scene;
+	rtScene.assets = &gAssets;
+
+	const uint32_t entCount = static_cast<uint32_t>( scene->entities.size() );
+	rtScene.lights.clear();
+	rtScene.models.clear();
+	rtScene.models.reserve( entCount );
+	rtScene.aabb = AABB();
+
+	for ( uint32_t i = 0; i < entCount; ++i )
+	{
+		RtModel rtModel;
+		CreateRayTraceModel( gAssets, scene->entities[ i ], &rtModel );
+		rtScene.models.push_back( rtModel );
+
+		AABB& aabb = rtModel.octree.GetAABB();
+		rtScene.aabb.Expand( aabb.GetMin() );
+		rtScene.aabb.Expand( aabb.GetMax() );
+	}
+
+	for ( uint32_t i = 0; i < MaxLights; ++i )
+	{
+		rtScene.lights.push_back( scene->lights[ i ] );
+	}
+
+	const uint32_t texCount = gAssets.textureLib.Count();
+	for ( uint32_t i = 0; i < texCount; ++i )
+	{
+		Asset<Texture>* texAsset = gAssets.textureLib.Find( i );
+		Texture& texture = texAsset->Get();
+
+		texture.cpuImage.Init( texture.info.width, texture.info.height );
+
+		for ( uint32_t py = 0; py < texture.info.height; ++py ) {
+			for ( uint32_t px = 0; px < texture.info.width; ++px ) {
+				RGBA rgba;
+				rgba.r = texture.bytes[ ( py * texture.info.width + px ) * 4 + 0 ];
+				rgba.g = texture.bytes[ ( py * texture.info.width + px ) * 4 + 1 ];
+				rgba.b = texture.bytes[ ( py * texture.info.width + px ) * 4 + 2 ];
+				rgba.a = texture.bytes[ ( py * texture.info.width + px ) * 4 + 3 ];
+				texture.cpuImage.SetPixel( px, py, rgba );
+			}
+		}
+	}
+}
+
+
+static void TraceScene( const bool rasterize = false )
+{
+	gImguiControls.raytraceScene = false;
+
+	rtview.targetSize[ 0 ] = 320;
+	rtview.targetSize[ 1 ] = 180;
+	//gWindow.GetWindowSize( rtview.targetSize[0], rtview.targetSize[1] );
+	Image<Color> rtimage( rtview.targetSize[ 0 ], rtview.targetSize[ 1 ], Color::Black, "testRayTrace" );
+	{
+		rtview.camera = rtScene.scene->camera;
+		rtview.viewTransform = rtview.camera.GetViewMatrix();
+		rtview.projTransform = rtview.camera.GetPerspectiveMatrix();
+		rtview.projView = rtview.projTransform * rtview.viewTransform;
+	}
+
+	if ( rasterize ) {
+		RasterScene( rtimage, rtview, rtScene, true );
+	}
+	else {
+		TraceScene( rtview, rtScene, rtimage );
+	}
+
+	{
+		std::stringstream ss;
+		const char* name = rtimage.GetName();
+		ss << "output/";
+		if ( ( name == nullptr ) || ( name == "" ) )
+		{
+			ss << reinterpret_cast<uint64_t>( &rtimage );
+		}
+		else
+		{
+			ss << name;
+		}
+
+		ss << ".bmp";
+
+		Bitmap bitmap = Bitmap( rtimage.GetWidth(), rtimage.GetHeight() );
+		ImageToBitmap( rtimage, bitmap );
+		bitmap.Write( ss.str() );
+	}
+}
