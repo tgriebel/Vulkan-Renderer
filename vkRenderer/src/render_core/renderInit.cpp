e4209690ea903acf82a081c7a9dd8949de9a6a5e
Thomas Griebel <631199+tgriebel@users.noreply.github.com>
2023-06-15 08:26:51 -0500

    Change vulkan name

--- vkRenderer/src/render_core/renderInit.cpp
+++ vkRenderer/src/render_core/renderInit.cpp
@@ -90,7 +90,7 @@ void Renderer::CreateInstance()
 
 	VkApplicationInfo appInfo{ };
 	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	appInfo.pApplicationName = "Hello Triangle";
+	appInfo.pApplicationName = "Extensia";
 	appInfo.applicationVersion = VK_MAKE_VERSION( 1, 0, 0 );
 	appInfo.pEngineName = "No Engine";
 	appInfo.engineVersion = VK_MAKE_VERSION( 1, 0, 0 );
