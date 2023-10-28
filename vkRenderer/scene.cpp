66245c8f76885e74480d8ff7dfeb9efad219d9bf
Thomas Griebel <631199+tgriebel@users.noreply.github.com>
2023-10-28 14:04:09 -0500

    Use ImGui table for shader debug

--- vkRenderer/scene.cpp
+++ vkRenderer/scene.cpp
@@ -360,6 +360,8 @@ void DrawSceneDebugMenu()
 void DrawAssetDebugMenu()
 {
 #if defined( USE_IMGUI )
+	static ImGuiTableFlags tableFlags = ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg;
+
 	if ( ImGui::BeginTabItem( "Assets" ) )
 	{
 		const uint32_t matCount = g_assets.materialLib.Count();
@@ -400,6 +402,7 @@ void DrawAssetDebugMenu()
 			}
 			ImGui::TreePop();
 		}
+
 		const uint32_t shaderCount = g_assets.gpuPrograms.Count();
 		if ( ImGui::TreeNode( "Shaders", "Shaders (%i)", shaderCount ) )
 		{
@@ -410,16 +413,30 @@ void DrawAssetDebugMenu()
 				const char* shaderName = shaderAsset->GetName().c_str();
 				if ( ImGui::TreeNode( shaderName ) )
 				{
-					for( uint32_t i = 0; i < shader.shaderCount; ++i )
+					if ( ImGui::BeginTable( shaderName, 3, tableFlags ) )
 					{
-						ImGui::Text( shader.shaders[ i ].name.c_str() );
-						switch( shader.shaders[ i ].type )
+						ImGui::TableSetupColumn( "Name" );
+						ImGui::TableSetupColumn( "Type" );
+						ImGui::TableSetupColumn( "Bytes" );
+						ImGui::TableHeadersRow();
+
+						for ( uint32_t i = 0; i < shader.shaderCount; ++i )
 						{
-							case VERTEX: ImGui::Text( "Vertex" ); break;
-							case PIXEL: ImGui::Text( "Pixel" ); break;
-							case COMPUTE: ImGui::Text( "Compute" ); break;
+							ImGui::TableNextRow();
+							ImGui::TableSetColumnIndex( 0 );
+							ImGui::Text( shader.shaders[ i ].name.c_str() );
+							ImGui::TableSetColumnIndex( 1 );
+							switch ( shader.shaders[ i ].type )
+							{
+								case VERTEX: ImGui::Text( "Vertex" ); break;
+								case PIXEL: ImGui::Text( "Pixel" ); break;
+								case COMPUTE: ImGui::Text( "Compute" ); break;
+							}
+							ImGui::TableSetColumnIndex( 2 );
+							ImGui::Text( "%u", shader.shaders[ i ].blob.size() );				
 						}
-						ImGui::Text( "Size: %u", shader.shaders[ i ].blob.size() );
+
+						ImGui::EndTable();
 					}
 					ImGui::TreePop();
 				}
