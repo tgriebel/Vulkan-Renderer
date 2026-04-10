e8bcda3899e286f5e1a30e942335a46e1599b24e
Thomas Griebel <631199+tgriebel@users.noreply.github.com>
2026-04-10 10:03:41 -0500

    Cleanup for VS2026

--- vkRenderer/src/render_binding/imageSampler.h
+++ vkRenderer/src/render_binding/imageSampler.h
@@ -24,33 +24,22 @@
 */
 
 #include <GfxCore/asset_types/texture.h>
-#include <SysCore/bitArray.h>
 #include "../render_core/gpuImage.h"
 #include "../render_state/deviceContext.h"
 
-using BaseImageArray = Array<const Image*, MaxImageDescriptors>;
-
-class ImageArray : public BaseImageArray
+class ImageSampler : public RenderResource
 {
 private:
 	uint64_t		m_lastFrameUpdate[MaxImageDescriptors];
 	RenderContext* m_context;
 
 public:
-	ImageArray()
+	ImageSampler()
 	{
 		m_context = nullptr;
 
 		memset(m_lastFrameUpdate, 0, MaxImageDescriptors * sizeof(uint64_t));
 	}
-
-	const Image* operator[](uint32_t index) const
-	{
-		return BaseImageArray::operator[](index);
-	}
-
-	// This name makes the intent more explicit than [] and allows additional book-keeping easily
-	void BindIndex(const uint32_t index, const Image* image);
 
 	void SetRenderContext(RenderContext* context);
 
