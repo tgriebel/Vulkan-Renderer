fe1c7e36cb97b84a3f20d95c53714f497981c069
Thomas Griebel <631199+tgriebel@users.noreply.github.com>
2026-05-31 08:47:46 -0500

    Cleanup `image shader` name
    It can be inferred from the bindset field

--- vkRenderer/scenes/sceneParser.cpp
+++ vkRenderer/scenes/sceneParser.cpp
@@ -690,7 +690,7 @@ int ParseShaderObject( parseState_t& st, void* object, uint32_t offset )
 	shaderFlags_t shaderFlags = shaderFlags_t::NONE;
 	AssetLib<GpuProgram>* shaders = reinterpret_cast<AssetLib<GpuProgram>*>( object );
 
-	const uint32_t objectCount = 15;
+	const uint32_t objectCount = 14;
 	const objectTuple_t objectMap[ objectCount ] =
 	{
 		{ "name",			&name,											TOKEN_LEN,					1,	&ParseStringObject },
@@ -706,11 +706,14 @@ int ParseShaderObject( parseState_t& st, void* object, uint32_t offset )
 		{ "bindset",		&bindSet,										TOKEN_LEN,					1,	&ParseStringObject },
 		{ "perms",			&perms,											TOKEN_LEN,					1,	&ParseStringObject },	// NOTE: works for arrays via ParseArray string-element path
 		{ "sampling_ms",	&shaderFlags,									sizeof( shaderFlags_t ),	1,	&ParseFlagObject<(uint32_t)shaderFlags_t::USE_MSAA> },
-		{ "image_shader",	&shaderFlags,									sizeof( shaderFlags_t ),	1,	&ParseFlagObject<(uint32_t)shaderFlags_t::IMAGE_SHADER> },
 		{ "no_vb",			&shaderFlags,									sizeof( shaderFlags_t ),	1,	&ParseFlagObject<(uint32_t)shaderFlags_t::NO_VERTEX_BUFFER> }
 	};
 
 	ParseObject( st, objectMap, objectCount );
+
+	if ( strcmp( bindSet, "bindset_imageShader" ) == 0 ) {
+		shaderFlags |= shaderFlags_t::IMAGE_SHADER;
+	}
 
 	GpuProgramLoader* loader = new GpuProgramLoader();
 	loader->SetSourcePath( "shaders/" );
